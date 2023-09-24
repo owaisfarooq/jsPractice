ea967ba86c7ac7bb3af9502e7568cf6a0a4212d4
owaisfarooq <owais.farooq.qureshi@gmail.com>
2023-09-24 18:20:26 +0500

    Time

--- getLastDigit.js
+++ getLastDigit.js
@@ -1,8 +1,11 @@
 const Test = require('./functionTesting');
 
-Test.assertEquals(getLastDigit(193150), 5);
+// Test.assertEquals(addTwoNumbers("893427328497983427893248932498034289324", "234859234879342897893427893274"), "893427328732842662772591830391462182598");
+
+// Test.assertEquals(getLastDigit(193150), 5);
 Test.assertEquals(getLastDigit(300), 0);
 Test.assertEquals(getLastDigit(20001), 6);
+// Test.assertEquals(getLastDigit(15), 0);
 
 // As you probably know, Fibonacci sequence are the numbers in the following integer
 // sequence: 1, 1, 2, 3, 5, 8, 13... 
@@ -12,19 +15,46 @@ Test.assertEquals(getLastDigit(20001), 6);
 // so, please, be careful with overflow.
 
 function addTwoNumbers(num1, num2) {
-    for (let i = num1.length-1; i >= 0; i--) {
-        // if ()
-        const digit1 = num1[i];
-        const digit2 = num2[i];
+    let carry = 0;
+    let result = [];
+
+    // Convert num1 and num2 to strings
+    num1 = num1.toString();
+    num2 = num2.toString();
+
+    // Make sure both numbers have the same length by padding with leading zeros if necessary
+    const maxLength = Math.max(num1.length, num2.length);
+    num1 = num1.padStart(maxLength, '0');
+    num2 = num2.padStart(maxLength, '0');
+
+    for (let i = maxLength - 1; i >= 0; i--) {
+        const digit1 = parseInt(num1[i]);
+        const digit2 = parseInt(num2[i]);
+
+        const sum = digit1 + digit2 + carry;
+        const newDigit = sum % 10;
+        carry = Math.floor(sum / 10);
+
+        result.unshift(newDigit); // Add the new digit to the beginning of the result array
     }
+
+    if (carry > 0) {
+        result.unshift(carry); // If there's a final carry, add it to the result
+    }
+
+    return result.join(''); // Convert the result array to a string
 }
 
 function getLastDigit(index) {
-    const sequence = [ '1', '1', '2', '3', '5', '8', '13'];
-    for (let i = 1; i < index; i++) {
-        sequence.push(addTwoNumbers(sequence.length-1, sequence.length-2));
+    // const sequence = [ '0', '1', '1', '2', '3', '5', '8', '13'];
+    const sequence = [ '0', '1'];
+
+    for (let i = 0; sequence.length <= index; i++) {
+        console.clear()
+        console.log(sequence.length, " done out of ", index);
+        sequence.push(addTwoNumbers(sequence[sequence.length-1], sequence[sequence.length-2]));
     }
-    return Number(sequence.pop());
+    return Number(sequence[sequence.length-1][sequence[sequence.length-1].length-1]);
 }
 
 
---
+++ to24hourtime.js
@@ -0,0 +1,56 @@
+const assert = require('./functionTesting');
+
+assert.strictEqual(to24hourtime( 1,  0, "am"), "0100", "Input =  1:00 am");
+assert.strictEqual(to24hourtime( 1,  0, "pm"), "1300", "Input =  1:00 pm");
+assert.strictEqual(to24hourtime(12,  0, "am"), "0000", "Input = 12:00 am");
+assert.strictEqual(to24hourtime(12,  0, "pm"), "1200", "Input = 12:00 pm");
+assert.strictEqual(to24hourtime( 6, 30, "am"), "0630", "Input =  6:30 am");
+assert.strictEqual(to24hourtime( 9, 45, "pm"), "2145", "Input =  9:45 pm");
+
+
+// Converting a 12-hour time like "8:30 am" or "8:30 pm" to 24-hour time (like "0830" or "2030") sounds easy enough, right? Well, let's see if you can do it!
+
+// You will have to define a function, which will be given
+// an hour (always in the range of 1 to 12, inclusive),
+// a minute (always in the range of 0 to 59, inclusive),
+// a period (either a.m. or p.m.) as input.
+
+// Your task is to return a four-digit string that encodes that time in 24-hour time.
+
+// Notes
+// By convention, noon is 12:00 pm, and midnight is 12:00 am.
+// On 12-hours clock, there is no 0 hour, and time just after midnight is denoted as, for example, 12:15 am. On 24-hour clock, this translates to 0015.
+
+
+
+// hour will always range from 1 to 12 (inclusive)
+// minute will always range from 0 to 59 (inclusive)
+// period will always be either "am" or "pm"
+
+
+
+function to24hourtime(hour, minute, period) {
+    let result = String(hour)
+    if (period !== 'am') {
+        result = String(hour+12);
+    }
+    if ( result.length < 2 ) {
+        result += "0";
+    }
+
+    if ( Number(result) > 12) {
+        result = result.split("").reverse().join("");
+    }
+
+    if ( String(minute).length < 2) {
+        result += "0"
+    }
+    result += minute;
+    return result;
+}
+
+
+
+
+// Converting 12-hour time to 24-hour time
+// https://www.codewars.com/kata/59b0a6da44a4b7080300008a/train/javascript
